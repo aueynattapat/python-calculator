cdeb1c52ea106983f837c95060beec3ac6dc8471
AueyAuey <109589098+aueynattapat@users.noreply.github.com>
2024-11-09 09:10:02 +0000

    Added test Medthod

--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,29 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
+    def test_add(self): #a + b
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+        self.assertEqual(self.calc.add(18, 18), 36)
+        self.assertEqual(self.calc.add(18, 2), 20)
+
+    def test_subtract(self): 
+        self.assertEqual(self.calc.subtract(55, 15), 40)
+        self.assertEqual(self.calc.subtract(27, 18), 9)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5, 8), 40)
+        self.assertEqual(self.calc.multiply(12, 9), 108)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(18, 6), 3)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(18, 5), 3)
+        self.assertEqual(self.calc.modulo(27, 3), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
